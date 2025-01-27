1c9486b963d0f3fbf496a91ddbf2a1c7433acd7b
preethi12-tes <preethit@testvagrant.com>
2025-01-27 14:41:51 +0530

    validating cart contents

---
+++ pages/cartPage.js
@@ -0,0 +1,34 @@
+export class cartPage {
+  constructor(page) {
+    this.cartIcon = page.locator(`//*[@class='icon icon-cart']`);
+    this.cartItems = page.locator(`//*[@id='main-cart-items']`);
+    this.productName = page.locator(`//*[@id='main-cart-items']//td[2]//a`);
+    this.productQuantity = page.locator(
+      `//div[@class='cart__items']/descendant::input[@id='Quantity-1']`
+    );
+    this.productPrice = page.locator(`//div[@class='totals']/p`);
+  }
+  async validateCartContents() {
+    await this.cartIcon.click();
+    const cartItemsCount = await this.cartItems.count();
+    if (cartItemsCount === 0) {
+      throw new Error("No products found in the cart");
+    }
+
+    let cartDetails = [];
+
+    for (let i = 0; i < cartItemsCount; i++) {
+      const itemName = await this.productName.nth(i).textContent();
+      const itemQuantity = await this.productQuantity.nth(i).inputValue();
+      const itemPrice = await this.productPrice.nth(i).textContent();
+
+      cartDetails.push({
+        name: itemName.trim(),
+        quantity: itemQuantity.trim(),
+        price: itemPrice.trim()
+      });
+    }
+
+    return cartDetails; 
+  }
+}
--- tests/e2eTest.spec.js
+++ tests/e2eTest.spec.js
@@ -1,11 +1,16 @@
 const { test, expect } = require("@playwright/test");
 const { loginPage } = require("../pages/loginPage");
 const { productsPage } = require("../pages/productPage");
-
+const { cartPage } = require("../pages/cartPage");
+const productData = JSON.parse(
+  JSON.stringify(
+    require("/Users/testvagrant/Desktop/CODE_TASK/testData/productData.json")
+  )
+);
 test("Login", async ({ page }) => {
   const LoginPage = new loginPage(page);
   const ProductsPage = new productsPage(page);
-
+ const CartPage= new cartPage(page)
   await LoginPage.goTo();
   await ProductsPage.addProduct();
   try {
@@ -21,4 +26,11 @@ test("Login", async ({ page }) => {
   await ProductsPage.getCartCount();
   const updatedCartCountText = await ProductsPage.cartIcon.textContent();
   expect(updatedCartCountText).toContain("1 item");
+  // validating cart contents
+  const cartItemsDetails = await CartPage.validateCartContents();
+
+    // Validate the first product in the cart
+    expect(cartItemsDetails[0].name).toBe(productData.productName); 
+    expect(cartItemsDetails[0].quantity).toBe(productData.productQuantity);  
+    expect(cartItemsDetails[0].price).toBe(productData.productPrice);  
 });
